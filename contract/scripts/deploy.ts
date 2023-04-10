6e3cf439a9a69b12d1ce6b863ef058932ae91a69
owen-hwlee <hwolee@connect.ust.hk>
2023-04-10 18:25:47 +0800

    Update deploy.ts

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -1,17 +1,22 @@
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
-async function deploy() {
-  // Search for contract in local directory
-  const Contract = await ethers.getContractFactory("");
-  const contract = await Contract.deploy();
+// TODO: add name of contracts to be deployed
+// TODO: determine if contracts should be deployed in order
+const contractsToDeploy: string[] = [
+  // "",
+];
 
-  await contract.deployed();
-  console.log("Contract deployed to:", contract.address);
+async function deploy() {
+  for (const contractName of contractsToDeploy) {
+    const Contract = await ethers.getContractFactory(contractName);
+    const contract = await Contract.deploy();
+    await contract.deployed();
+
+    console.log("Contract deployed to:", contract.address);
+  }
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 deploy().catch((error) => {
   console.error(error);
   process.exitCode = 1;
