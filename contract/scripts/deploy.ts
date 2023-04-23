a663890c44723ec398e247b514a3486b85104818
owen-hwlee <hwolee@connect.ust.hk>
2023-04-23 08:51:04 +0800

    Refactored deploy.ts for readability

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -1,60 +1,69 @@
-import { Contract } from "ethers";
 import fs from "fs";
 import { ethers, network } from "hardhat";
 import path from "path";
 
 // Name of contracts to deploy
-const USERS_CONTRACT = "Users";
+const ISSUER_STORAGE = "IssuerStorage";
+const APPLICANT_STORAGE = "ApplicantStorage";
+const ACCREDITATION_STORAGE = "AccreditationStorage";
+const CERTIFICATE_STORAGE = "CertificateStorage";
 const ACCREDITATION_NFT = "AccreditationNFT";
 const CERTIFICATE_NFT = "CertificateNFT";
 
 /**
  * This function deploys the contracts to blockchain for use
  * - Executed in npm commands `deploy`(for PROD), `testnet`(for test), `local`(for local)
+ * - Blockchain network deployed to is chosen by the npm command executed
+ * - Blockchain networks are configured in `hardhat.config.ts`
+ * 
+ * TODO: Update this
+ * Flow of deployment:
+ * 1. Deploy storage contracts
+ *   - IssuerStorage.sol
+ *   - ApplicantStorage.sol
+ *   - AccreditationStorage.sol
+ *     - Dependent on IssuerStorage.sol
+ *     - May be dependent on Set.sol
+ *   - CertificateStorage.sol
+ *     - Dependent on IssuerStorage.sol, ApplicantStorage.sol, AccreditationStorage.sol
+ *     - May be dependent on Set.sol
+ * 2. Deploy NFT contracts
+ *   - AccreditationNFT.sol
+ *     - Dependent on AccreditationStorage.sol, CertificateStorage.sol
+ *   - CertificateNFT.sol
+ *     - Dependent on AccreditationStorage.sol, CertificateStorage.sol
+ *
+ * Note: deployment of types and libraries are not required
  */
 async function deploy() {
   // Stores deployment logs
   const logs: string[] = [
     `Deploying on network "${network.name}" at RPC URL ${network.config.url}`,
   ];
-  // Helper function to add a deployment log to `logs`
-  const createDeployLog = (contractName: string, address: string) => {
+
+  // Helper function to deploy a contract and return deployment address
+  const deployContractGetAddress = async (contractName: string, ...deployArgs: string[]) => {
+    console.log("Deploying:", contractName);
+    const Contract = await ethers.getContractFactory(contractName);
+    const contract = await Contract.deploy(...deployArgs);
+    await contract.deployed();
+
+    const address = contract.address;
+    // Add deployment log to `logs` array
     const log = `Contract ${contractName} deployed to address ${address} at ${new Date().toUTCString()}`;
     logs.push(log);
     console.log(log);
+    return address;
   };
 
-  // 1. Deploy User contract
-  console.log("Deploying:", USERS_CONTRACT);
-  const UsersContract = await ethers.getContractFactory(USERS_CONTRACT);
-  const usersContract = await UsersContract.deploy();
-  await usersContract.deployed();
-
-  // Get deployed users contract address
-  const usersAddress = usersContract.address;
-  createDeployLog(USERS_CONTRACT, usersAddress);
-
-  // 2. Deploy Accreditation contract after obtaining User contract address
-  console.log("Deploying:", ACCREDITATION_NFT);
-  const AccreditationContract = await ethers.getContractFactory(
-    ACCREDITATION_NFT,
-  );
-  const accreditationContract = await AccreditationContract.deploy(usersAddress);
-  await accreditationContract.deployed();
-
-  // Get deployed accreditation contract address
-  const accreditationAddress = accreditationContract.address;
-  createDeployLog(ACCREDITATION_NFT, accreditationAddress);
-
-  // 3. Deploy Certificate contract after obtaining Accreditation contract address
-  const CertificateContract = await ethers.getContractFactory(CERTIFICATE_NFT);
-  const certificateContract = await CertificateContract.deploy(accreditationAddress);
-  await certificateContract.deployed();
-
-  // Get deployed certificate contract address
-  const certificateAddress = certificateContract.address;
-  createDeployLog(CERTIFICATE_NFT, certificateAddress);
-  console.log(CERTIFICATE_NFT, "contract deployed to:", certificateAddress);
+  // Deploy contracts sequentially, input dependent contract addresses as argument
+  // TODO: decide which addresses to input as argument
+  const issuerStorageAddress = await deployContractGetAddress(ISSUER_STORAGE);
+  const applicantStorageAddress = await deployContractGetAddress(APPLICANT_STORAGE);
+  const accreditationStorageAddress = await deployContractGetAddress(ACCREDITATION_STORAGE);
+  const certificateStorageAddress = await deployContractGetAddress(CERTIFICATE_STORAGE);
+  const accreditationNFTAddress = await deployContractGetAddress(ACCREDITATION_NFT);
+  const certificateNFTAddress = await deployContractGetAddress(CERTIFICATE_NFT);
 
   // Save deployed addresses to local log files for reference
   const fileString: string = logs.join("\n");
