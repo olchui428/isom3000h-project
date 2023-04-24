ed72831a027b19b63720257eff5091c79f7222ca
owen-hwlee <hwolee@connect.ust.hk>
2023-04-25 00:23:33 +0800

    Added new Endpoint contracts

---
+++ contract/contracts/endpoints/ApplicantEndpoint.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import { Accreditation } from "../types/nft/Accreditation.sol";
+import { Certificate } from "../types/nft/Certificate.sol";
+
+contract ApplicantEndpoint {
+    // ========================= Variables =========================
+
+    // -------------------- Contract addresses --------------------
+
+    /// @dev Wallet address of deployer, similar to admin address
+    address payable private _deployerAddress;
+
+    // /// @dev Address of deployed ApplicantStorage contract
+    // address private _applicantStorageAddress;
+
+    // -------------------- Variables --------------------
+
+    // -------------------- Contracts --------------------
+
+    // /// @dev Storage contract for Issuers
+    // IssuerStorage issuerStorage;
+    // /// @dev Storage contract for Applicants
+    // ApplicantStorage applicantStorage;
+
+    // TODO: add Contracts
+
+    // ========================= Functions & Modifiers =========================
+
+    // -------------------- Setting up contracts --------------------
+
+    /// @notice Deploys a storage contract for Accreditation
+    /// @param issuerStorageAddress: think add what parameters, aka need to which contracts this contract will call
+    constructor(string memory issuerStorageAddress) {
+        _deployerAddress = payable(msg.sender);
+        issuerStorageAddress;
+    }
+
+    // -------------------- Functions --------------------
+
+    // TODO: add endpoint functions
+}
