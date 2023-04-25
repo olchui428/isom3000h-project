d982251c927e4b6a45d2afed664a9d0d980ae7b1
owen-hwlee <hwolee@connect.ust.hk>
2023-04-25 19:53:28 +0800

    Added missing pages to front-end

---
+++ frontend/src/pages/certificate/create.tsx
@@ -0,0 +1,8 @@
+function CertificateCreate() {
+  // TODO: only authorize Issuer enter this page
+  // TODO: add form to allow creation of new certificate
+  // TODO: redirect to certificate page of that new certificate after successfully create
+  return <></>;
+}
+
+export default CertificateCreate;
