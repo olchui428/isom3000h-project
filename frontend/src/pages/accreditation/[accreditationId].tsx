5b2f99b8f24ecf749d8ffc0b2edd2580bc3c1bbf
owen-hwlee <hwolee@connect.ust.hk>
2023-04-28 17:03:43 +0800

    Update [accreditationId].tsx

--- frontend/src/pages/accreditation/[accreditationId].tsx
+++ frontend/src/pages/accreditation/[accreditationId].tsx
@@ -1,4 +1,9 @@
+import useMetaMask from "@/hooks/useMetaMask";
+import { useRouter } from "next/router";
+
 function AccreditationById() {
+  const accreditationId = parseInt(useRouter().query.accreditationId as string);
+  const accreditationData = useMetaMask().AccreditationEndpoint().getAccreditationById(accreditationId);
   // TODO: display all information of this accreditation
   // TODO: add toggle to display all certificates of this accreditation, need call endpoint
   // TODO: add link to corresponding issuer (optional: need new page)
