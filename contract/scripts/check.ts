35b80bc50389688ac2287a606da685f1fc2c1cdd
Chui On Lam <olchui@connect.ust.hk>
2023-05-11 14:47:46 +0800

    done checks, modified .env.example

--- contract/scripts/check.ts
+++ contract/scripts/check.ts
@@ -1,17 +1,37 @@
 import fs from "fs";
-import { Contract, Wallet } from "ethers";
+import { Contract, Wallet, utils } from "ethers";
 import { ethers, network } from "hardhat";
 import path from "path";
+import * as dotenv from "dotenv";
 import issuerEndpointJSON from "../artifacts/contracts/endpoints/IssuerEndpoint.sol/IssuerEndpoint.json";
 import applicantEndpointJSON from "../artifacts/contracts/endpoints/ApplicantEndpoint.sol/ApplicantEndpoint.json";
 import accreditationEndpointJSON from "../artifacts/contracts/endpoints/AccreditationEndpoint.sol/AccreditationEndpoint.json";
 import certificateEndpointJSON from "../artifacts/contracts/endpoints/CertificateEndpoint.sol/CertificateEndpoint.json";
 
+/*
+ * If testing in local network (Ganache),
+ * set ENV = "LOCAL",
+ * replace LOCAL_ENDPOINTSs and LOCAL_PKs in .env file.
+ *
+ * If testing in testnet (Mumbai Testnet),
+ * set ENV = "TESTNET",
+ * replace TEST_ENDPOINTSs and TEST_PKs in .env file.
+ */
+
+// Environment now (LOCAL || TESTNET || PROD)
+const ENV = "LOCAL";
+
 // Addresses of deployed endpoint contracts
-const ISSUER_ENDPOINT = "0xB579a3D8B2F541c04ED575ED160F6Ca28D30DdE6";
-const APPLICANT_ENDPOINT = "0x064a8093C5986A6d243C75795068BFfA2b6384A1";
-const ACCREDITATION_ENDPOINT = "0x9844F3693997D1e4e4e52F8f507DeB7babBc97D5";
-const CERTIFICATE_ENDPOINT = "0xe469C0a13D58c602f6117c7b553904aF5AF61330";
+const ISSUER_ENDPOINT =
+  ENV == "LOCAL" ? process.env.LOCAL_ISSUER_ENDPOINT : process.env.TEST_ISSUER_ENDPOINT;
+const APPLICANT_ENDPOINT =
+  ENV == "LOCAL" ? process.env.LOCAL_APPLICANT_ENDPOINT : process.env.TEST_APPLICANT_ENDPOINT;
+const ACCREDITATION_ENDPOINT =
+  ENV == "LOCAL"
+    ? process.env.LOCAL_ACCREDITATION_ENDPOINT
+    : process.env.TEST_ACCREDITATION_ENDPOINT;
+const CERTIFICATE_ENDPOINT =
+  ENV == "LOCAL" ? process.env.LOCAL_CERTIFICATE_ENDPOINT : process.env.TEST_CERTIFICATE_ENDPOINT;
 
 // Import abis
 const issuerEndpointAbi = issuerEndpointJSON.abi;
@@ -19,10 +39,47 @@ const applicantEndpointAbi = applicantEndpointJSON.abi;
 const accreditationEndpointAbi = accreditationEndpointJSON.abi;
 const certificateEndpointAbi = certificateEndpointJSON.abi;
 
-// Local network private keys, copy over from Ganache
-const issuerPK = "0x29d5d48921437ce41b7055334a2a39218e9ffd64ea4f57e9099957128cb86244";
-const applicant1PK = "0x9f6e9baa3c42952325d2c9d723829423bb832f7f34faa51c51b9de662df3e7c7";
-const applicant2PK = "0xe2dd591f223d0d7857f78be8f64aa8b8c8f10ce0bb26c5422fdad47ab467704c";
+// Local network private keys, copy over from Ganache || Testnet private keys
+const issuerPK = ENV == "LOCAL" ? process.env.LOCAL_PK : process.env.TEST_PK;
+const applicant1PK = ENV == "LOCAL" ? process.env.LOCAL_PK_1 : process.env.TEST_PK_1;
+const applicant2PK = ENV == "LOCAL" ? process.env.LOCAL_PK_2 : process.env.TEST_PK_2;
+
+// Dummy variables
+const _issuer = {
+  name: "ABC Company",
+  description: "It is a good company",
+  logoUrl: "https://picsum.photos/200/300",
+};
+const _applicant1 = {
+  name: "Owen Lee",
+};
+const _applicant2 = {
+  name: "Amy Chan",
+};
+const _accreditation = {
+  id: -1,
+  title: "HKDSE",
+  createdAt: Date.now(),
+  duration: 1,
+  nature: "Exam",
+  description: "It is a tough exam.",
+};
+const _certificate1 = {
+  id: -1,
+  createdAt: Date.now(),
+  accreditationId: -1,
+  level: "5**",
+  eventId: "777",
+  remarks: "Good",
+};
+const _certificate2 = {
+  id: -1,
+  createdAt: Date.now(),
+  accreditationId: -1,
+  level: "2",
+  eventId: "778",
+  remarks: "Poor",
+};
 
 /**
  * This function checks the functionality of deployed contracts by simulating a workflow on the local chain
@@ -92,58 +149,98 @@ async function check() {
 
     // Start checking
     createLog("Checking IssuerEndpoint::registerIssuer()");
-    const registerIssuerTx = await issuerEndpointContract.connect(issuer).registerIssuer();
+    const registerIssuerTx = await issuerEndpointContract
+      .connect(issuer)
+      .registerIssuer(_issuer.name, _issuer.description, _issuer.logoUrl);
     const registerIssuerReceipt = await registerIssuerTx.wait();
     createLog("Successfully registered Issuer");
-    createLog("Registered Issuer: " + (await issuerEndpointContract.getIssuerByAddress()));
+    createLog(
+      "Registered Issuer: " + (await issuerEndpointContract.getIssuerByAddress(issuer.address))
+    );
 
     createLog("Checking ApplicantEndpoint::registerApplicant()");
     const registerApplicant1Tx = await applicantEndpointContract
       .connect(applicant1)
-      .registerApplicant();
+      .registerApplicant(_applicant1.name);
     const registerApplicant1Receipt = await registerApplicant1Tx.wait();
     createLog("Successfully registered Applicant 1");
     const registerApplicant2Tx = await applicantEndpointContract
       .connect(applicant2)
-      .registerApplicant();
+      .registerApplicant(_applicant2.name);
     const registerApplicant2Receipt = await registerApplicant2Tx.wait();
     createLog("Successfully registered Applicant 2");
     createLog(
-      "Registered Applicant 1: " + (await applicantEndpointContract.getApplicantByAddress())
+      "Registered Applicant 1: " +
+        (await applicantEndpointContract.getApplicantByAddress(applicant1.address))
     );
     createLog(
-      "Registered Applicant 2: " + (await applicantEndpointContract.getApplicantByAddress())
+      "Registered Applicant 2: " +
+        (await applicantEndpointContract.getApplicantByAddress(applicant2.address))
     );
 
     createLog("Checking AccreditationEndpoint::launchAccreditation()");
     const launchAccreditationTx = await accreditationEndpointContract
       .connect(issuer)
-      .launchAccreditation();
+      .launchAccreditation(
+        _accreditation.title,
+        _accreditation.createdAt,
+        _accreditation.duration,
+        _accreditation.nature,
+        _accreditation.description
+      );
     const launchAccreditationReceipt = await launchAccreditationTx.wait();
     createLog("Successfully launched Accreditation");
+    const launchAccreditationReceiptData = utils.defaultAbiCoder.decode(
+      ["uint256", "address", " string", "uint256", "uint256", "string", "string"],
+      launchAccreditationReceipt.logs[1].data
+    );
+    _accreditation.id = launchAccreditationReceiptData[0];
+    _certificate1.accreditationId = _accreditation.id;
+    _certificate2.accreditationId = _accreditation.id;
     createLog(
-      "Launched Accreditation: " + (await accreditationEndpointContract.getAccreditationById())
+      "Launched Accreditation: " +
+        (await accreditationEndpointContract.getAccreditationById(_accreditation.id))
     );
 
     createLog("Checking CertificateEndpoint::issueCertificate()");
     const issueCertificate1Tx = await certificateEndpointContract
       .connect(issuer)
-      .issueCertificate();
+      .issueCertificate(
+        applicant1.address,
+        _certificate1.createdAt,
+        _certificate1.accreditationId,
+        _certificate1.level,
+        _certificate1.eventId,
+        _certificate1.remarks
+      );
     const issueCertificate1Receipt = await issueCertificate1Tx.wait();
+    const issueCertificate1ReceiptData = utils.defaultAbiCoder.decode(
+      ["uint256", "uint256", "address", "address", "uint256", "string", "string", "string"],
+      issueCertificate1Receipt.logs[1].data
+    );
+    _certificate1.id = issueCertificate1ReceiptData[0];
     createLog("Successfully issued Certificate 1 to Applicant 1");
     const issueCertificate2Tx = await certificateEndpointContract
       .connect(issuer)
-      .issueCertificate();
+      .issueCertificate(
+        applicant2.address,
+        _certificate2.createdAt,
+        _certificate2.accreditationId,
+        _certificate2.level,
+        _certificate2.eventId,
+        _certificate2.remarks
+      );
     const issueCertificate2Receipt = await issueCertificate2Tx.wait();
+    const issueCertificate2ReceiptData = utils.defaultAbiCoder.decode(
+      ["uint256", "uint256", "address", "address", "uint256", "string", "string", "string"],
+      issueCertificate2Receipt.logs[1].data
+    );
+    _certificate2.id = issueCertificate2ReceiptData[0];
     createLog("Successfully issued Certificate 2 to Applicant 2");
 
-    const completeCert1 = await certificateEndpointContract.getCompleteCertById(
-      issueCertificate1Receipt.events[0].args.id
-    );
+    const completeCert1 = await certificateEndpointContract.getCompleteCertById(_certificate1.id);
     createLog("CompleteCert 1: " + completeCert1);
-    const completeCert2 = await certificateEndpointContract.getCompleteCertById(
-      issueCertificate2Receipt.events[0].args.id
-    );
+    const completeCert2 = await certificateEndpointContract.getCompleteCertById(_certificate2.id);
     createLog("CompleteCert 2: " + completeCert2);
 
     createLog(`\nChecking complete at ${new Date().toUTCString()}!`);
